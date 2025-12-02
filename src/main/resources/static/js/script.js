54250fe1d052d966fa70a37f764bf09e427b53af
KCWGH <kimzenio07@gmail.com>
2025-12-02 13:51:45 +0900

    fixed function 'getCsrfToken'

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -39,7 +39,19 @@ const SOCIAL_LOGIN_URLS = {
     user: `${BASE_URL}/api/user`
 };
 function getCsrfToken() {
-    return document.cookie.replace(/(?:(?:^|.*;\\s*)XSRF-TOKEN\\s*\\=\\s*([^;]*).*$)|^.*$/, "$1");
+    const name = 'XSRF-TOKEN=';
+    const decodedCookie = decodeURIComponent(document.cookie);
+    const ca = decodedCookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
 }
 function initAudioContext() {
     if (!audioContext) {
